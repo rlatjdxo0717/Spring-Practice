e793dea397ed109ec3fdbd8eaab532582c6f88cf
rlatjdxo0717 <78012924+rlatjdxo0717@users.noreply.github.com>
2021-05-04 09:51:25 +0900

    Add files via upload

---
+++ spring40/src/main/java/com/mega/mvc40/BbsDAO.java
@@ -0,0 +1,28 @@
+package com.mega.mvc40;
+
+import java.util.List;
+
+import org.mybatis.spring.SqlSessionTemplate;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+@Component // BbsDAO dao = new BbsDAO(); 한개만 객체 생성!, 싱글톤!
+public class BbsDAO {
+
+	@Autowired
+	SqlSessionTemplate my;
+
+	public List<BbsVO> all() {
+		return my.selectList("bbs.all");
+	}
+
+	public void create(BbsVO bag) throws Exception { // VO는 프로토타입이라고 볼수 있다. set메서드를 해주므로.
+		my.insert("bbs.insert", bag);
+	}
+
+	public BbsVO read(BbsVO bag) { // VO는 프로토타입이라고 볼수 있다. set메서드를 해주므로.
+		BbsVO vo = my.selectOne("bbs.select", bag);
+		return vo;
+	}
+
+}
